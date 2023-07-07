4cb3bcb01969cd8858c0c2ffe70dfb6d12d07529
akapu <alexkap544@gmail.com>
2023-07-07 16:45:21 +0300

    generates runningtext, but with issues

--- main.py
+++ main.py
@@ -8,22 +8,48 @@ def main():
 
     FPS = 60
     DURATION = 3
+    TOTAL_FRAMES = DURATION * FPS
 
     FILENAME = "running_text.mp4"
     CODECNAME = "vp09"
 
-    background = np.zeros((WIDTH, HEIGHT, LAYERS), dtype=np.uint8)
-
-    fourcc = cv2.VideoWriter_fourcc(*CODECNAME)
+    FONTSCALE = 3
+    FONT = cv2.FONT_HERSHEY_DUPLEX
+    THICKNESS = 1
+    TEXT = "it-solution.ru akapu"
+    TEXT_SIZE, _ = cv2.getTextSize(
+        TEXT,
+        FONT,
+        FONTSCALE,
+        THICKNESS
+    )
+    H_PAD = 5
+    BOTTOM_PAD = 5
 
     video = cv2.VideoWriter(
         FILENAME,
-        fourcc,
+        cv2.VideoWriter_fourcc(*CODECNAME),
         FPS,
-        (WIDTH, HEIGHT))
+        (WIDTH, HEIGHT)
+    )
 
-    for i in range(DURATION * FPS):
-        video.write(background)
+    for i in range(TOTAL_FRAMES):
+        img = np.zeros((WIDTH, HEIGHT, LAYERS), dtype=np.uint8)
+
+        shift = int((i / (TOTAL_FRAMES - 1)) * (WIDTH - TEXT_SIZE[0] - 2*H_PAD))
+
+        cv2.putText(
+            img,
+            TEXT,
+            (H_PAD + shift, HEIGHT - BOTTOM_PAD),
+            FONT,
+            FONTSCALE,
+            (255, 255, 255),
+            THICKNESS,
+            cv2.FILLED
+        )
+
+        video.write(img)
 
     video.release()
 
